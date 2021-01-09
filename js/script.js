cb8d7cc3a3269769000da54c54d765101f2441d5
ledeDEV02 <vladimirlebedev00010@gmail.com>
2021-01-09 11:20:34 +0500

    improved the app by adding loops

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 "use scrict";
 
-const numberOfFilms = prompt("Сколько фильмов уже смотрели?");
+const numberOfFilms = +prompt("Сколько фильмов уже смотрели?");
 
 const personalMovieDB = {
 	count: numberOfFilms,
@@ -9,13 +9,23 @@ const personalMovieDB = {
 	genres: [],
 	privat: false
 };
+for (let i = 0; i < 2; i++) {
+	const lastFilm = prompt("Один из последних просмотренных фильмов?");
+	const mark = prompt("На сколько оцените его?");
+	while ((lastFilm.length === 0 || lastFilm.length > 50 || lastFilm == null) && (mark.length === 0 || mark.length > 50 || mark == null)) {
+		const lastFilm = prompt("Один из последних просмотренных фильмов?");
+		const mark = prompt("На сколько оцените его?");
+	}
+	personalMovieDB.movies[lastFilm] = mark;
+}
 
-const lastFilm = prompt("Один из последних просмотренных фильмов?");
-const mark1 = prompt("На сколько оцените его?");
-const preFilm = prompt("Один из последних просмотренных фильмов?");
-const mark2 = prompt("На сколько оцените его?");
-
-personalMovieDB.movies[lastFilm] = mark1;
-personalMovieDB.movies[preFilm] = mark2;
-
-console.log(personalMovieDB);
+if (personalMovieDB.count < 10) {
+	alert("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count < 31 && personalMovieDB.count > 9) {
+	alert("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+	alert("Вы - киноман!");
+}
+else {
+	alert("Произошла ошибка");
+}
